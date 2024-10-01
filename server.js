13a31b65c0430e5502ae7f2c22a0accee624eae3
Spoker76 <josuepadilla_13@outlook.com>
2024-10-01 13:08:39 -0600

    Modificado campo de obtener ticket por tramite

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.post('/api/tickets-per-process', async (req, res) => {
             orderedTickets = [...orderedTickets, ...otherPriorityTickets.slice(otherPriorityIndex)];
         }
 
-        res.status(201).send({ message: 'INFO:: Tickets obtenidos', result: orderedTickets });
+        res.status(201).send({ message: 'INFO:: Tickets obtenidos', result: orderedTickets[0] });
     } catch (error) {
         console.error('ERROR:: Error al obtener los tickets:', error);
         res.status(500).send('Error al obtener los tickets!');
